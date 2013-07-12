ae8724827218a706d30650c2f156fe29e29b963b
scanban <zvfs221@mail.ru>
2013-07-12 16:16:59 +0400

    Tree failing bolt example

---
+++ src/main/java/examples/TreeFailApp.java
@@ -0,0 +1,32 @@
+package examples;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+import examples.faults.FailAwareSpout;
+import examples.faults.FailingBolt;
+import examples.faults.MultiplierBolt;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class TreeFailApp
+{
+    public static void main( String[] args ) throws Throwable
+    {
+        Logger.getRootLogger().setLevel(Level.ERROR);
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("FailAwareSpout", new FailAwareSpout());
+        builder.setBolt("MultiplierBolt", new MultiplierBolt(), 2).shuffleGrouping("FailAwareSpout");
+        builder.setBolt("FailingBolt", new FailingBolt(), 2).shuffleGrouping("MultiplierBolt");
+
+        Config config = new Config();
+        config.setDebug(false);
+        //config.setNumWorkers(32);
+
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("T1", config, builder.createTopology());
+        Thread.sleep(1000*10);
+        cluster.shutdown();
+
+    }
+}
---
+++ src/main/java/examples/faults/MultiplierBolt.java
@@ -0,0 +1,34 @@
+package examples.faults;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+import java.util.Map;
+
+public class MultiplierBolt extends BaseRichBolt {
+    private static final int MULTI_COUNT = 3;
+    OutputCollector outputCollector;
+
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.outputCollector = outputCollector;
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        for(int i = 0; i < MULTI_COUNT; ++i) {
+            outputCollector.emit(tuple, new Values(tuple.getString(0) + " - " + i));
+        }
+        outputCollector.ack(tuple);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("msg"));
+    }
+}
